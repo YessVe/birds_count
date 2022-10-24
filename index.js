b9682988dc645b43c46510cdef0ed70cbb286a89
Yessenia <vela.kathy@gmail.com>
2022-10-24 18:48:11 -0500

    create increment function

---
+++ index.js
@@ -0,0 +1,11 @@
+let countText = document.getElementById("count-txt");
+let count = 0
+
+function increment(){
+    count = count + 1;
+    countText.innerText=count
+}
+
+function save() {
+    console.log('linea6');
+}
