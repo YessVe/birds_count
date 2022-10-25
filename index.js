7813da77e79ef4afc1d99662e91421d171eedb29
Yessenia <vela.kathy@gmail.com>
2022-10-24 19:31:41 -0500

    create start again function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let entriesText = document.getElementById("entries-txt");
 
 function increment(){
     count = count + 1;
-    countText.innerText=count
+    countText.innerText = count
 }
 
 function save() {
@@ -12,4 +12,10 @@ function save() {
     entriesText.textContent += entries;
     countText.innerText = 0;
     count = 0;
+}
+
+function startAgain() {
+    count = 0;
+    countText.innerText = 0;
+    entriesText.textContent = "Previous entries: ";
 }
